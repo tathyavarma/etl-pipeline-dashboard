60bec24e38f7cbea11d64263d97f68e65ef296fe
tathyavarma <tathyavarma@gmail.com>
2026-05-28 02:36:08 +0530

    Implement initial ETL application module and data models

---
+++ backend/apps/etl/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class PipelineRun(models.Model):
+    name = models.CharField(max_length=100)
+    status = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
